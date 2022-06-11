856441f67ebe5065ce1fa40414a6618ddc7c8bc7
joachimnorden <joachim.norden@gmail.com>
2022-06-11 19:48:45 +0000

    Add Posts to homepage

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'index.html', {})
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
