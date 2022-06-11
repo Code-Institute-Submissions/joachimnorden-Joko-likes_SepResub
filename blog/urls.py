d1589508c6e3fcca6a5c3a39d3555c1555030827
joachimnorden <joachim.norden@gmail.com>
2022-06-11 21:55:17 +0000

    Add a create post form and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PostView
+from .views import PostView, ArticleView, AddPostView
 
 urlpatterns = [
-    path('', PostView.as_view(), name="home")
+    path('', PostView.as_view(), name='home'),
+    path('post/<int:pk>', ArticleView.as_view(), name='post-detail'),
+    path('add-post', AddPostView.as_view(), name='add-post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 class PostView(ListView):
     model = Post
     template_name = 'index.html'
+
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'post.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add-post.html'
