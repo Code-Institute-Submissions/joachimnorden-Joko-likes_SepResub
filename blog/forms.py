660bc1e92d35385153ed23331f860072fef7ce8d
joachimnorden <joachim.norden@gmail.com>
2022-06-11 22:29:40 +0000

    Add edit and delete views and templates

--- blog/forms.py
+++ blog/forms.py
@@ -8,9 +8,22 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'subtitle', 'author', 'body')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'subtitle': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Title tag'}),
+            'subtitle': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Subtitle'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Start blogging...'}),
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'subtitle', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Title tag'}),
+            'subtitle': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Subtitle'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Start blogging...'}),
         }
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostView, ArticleView, AddPostView
+from .views import PostView, ArticleView, AddPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', PostView.as_view(), name='home'),
     path('post/<int:pk>', ArticleView.as_view(), name='post-detail'),
     path('add-post', AddPostView.as_view(), name='add-post'),
+    path('post/edit/<int:pk>', EditPostView.as_view(), name='edit-post'),
+    path('post/<int:pk>/delete', DeletePostView.as_view(), name='delete-post'),
 ]
