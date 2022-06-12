5251b47ea5847824b671349ed771ce62adea2243
joachimnorden <joachim.norden@gmail.com>
2022-06-12 12:18:37 +0000

    Add category to navbar and a category list page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostView, ArticleView, AddPostView, EditPostView, DeletePostView, AddCategoryView, CategoryView
+from .views import PostView, ArticleView, AddPostView, EditPostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     path('', PostView.as_view(), name='home'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('add-category', AddCategoryView.as_view(), name='add-category'),
     path('post/edit/<int:pk>', EditPostView.as_view(), name='edit-post'),
     path('post/<int:pk>/delete', DeletePostView.as_view(), name='delete-post'),
-    path('category/<str:category>/', CategoryView, name='category')
+    path('category/<str:category>/', CategoryView, name='category'),
+    path('category-list', CategoryListView, name='category-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,10 +10,22 @@ class PostView(ListView):
     template_name = 'index.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(PostView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
 
 class ArticleView(DetailView):
     model = Post
     template_name = 'post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
 
 
 class AddPostView(CreateView):
@@ -38,6 +50,11 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete-post.html'
     success_url = reverse_lazy('home')
+
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category-list.html', {'cat_menu_list':cat_menu_list})
 
 
 def CategoryView(request, category):
