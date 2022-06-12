08a148dde60ab64987468a4418d85fcf4cd3d42c
joachimnorden <joachim.norden@gmail.com>
2022-06-12 16:45:13 +0000

    Add image to models

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -17,6 +18,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = CloudinaryField('image', default='placeholder', null=True, blank=True)
     title_tag = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,6 +26,9 @@ class Post(models.Model):
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='gaming')
     likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    class Meta:
+        ordering = ['-post_date']
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
