0c117c7429165b11c8adfe97ea89961b81a3e72e
joachimnorden <joachim.norden@gmail.com>
2022-06-12 14:52:39 +0000

    Add unlike button

--- blog/views.py
+++ blog/views.py
@@ -27,8 +27,14 @@ class ArticleView(DetailView):
         context = super(ArticleView, self).get_context_data(*args, **kwargs)
         getlikes = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = getlikes.total_likes()
+
+        liked = False
+        if getlikes.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context['cat_menu'] = cat_menu
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 
@@ -69,5 +75,12 @@ def CategoryView(request, category):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:    
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
